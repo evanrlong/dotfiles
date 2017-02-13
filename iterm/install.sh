9aa182f142a48b20f2cc7959ae4877e3924a1157
Evan Long <evanlong19@gmail.com>
2017-02-12 20:56:39 -0700

    Removed iterm restarts from install and uninstall scripts.  Install/uninstall scripts should only be run in script/setup, followed by a system restart.

--- iterm/install.sh
+++ iterm/install.sh
@@ -1,19 +1,8 @@
 #!/bin/sh
 # Install script for iTerm 2 preferences
 
-pgrep -q iTerm && export ITERM_RUNNING=1
-
-if [ $ITERM_RUNNING ]; then
-	echo "test"
-  pkill iTerm
-fi
-
 defaults delete com.googlecode.iterm2
 rm ~/Library/Preferences/com.googlecode.iterm2.plist
 cp ~/.dotfiles/iterm/com.googlecode.iterm2.plist.default ~/Library/Preferences/com.googlecode.iterm2.plist
 defaults read com.googlecode.iterm2
 killall cfprefsd
-
-if [ $ITERM_RUNNING ]; then
-  open -a iTerm
-fi
